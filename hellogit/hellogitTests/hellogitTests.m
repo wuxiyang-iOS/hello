8171575fe27d56d6d883ec08461d5fef6ce6bc2c
wuxiyang-IOS <342534506@qq.com>
2018-03-22 12:07:46 +0800

    gitfirst

---
+++ hellogit/hellogitTests/hellogitTests.m
@@ -0,0 +1,39 @@
+//
+//  hellogitTests.m
+//  hellogitTests
+//
+//  Created by wuxiyang on 18/3/22.
+//  Copyright © 2018年 wuxiyang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hellogitTests : XCTestCase
+
+@end
+
+@implementation hellogitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
