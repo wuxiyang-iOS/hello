1b22c3be48a989dbaeb72e0ac0c7b49f7ba84423
wuxiyang-IOS <342534506@qq.com>
2018-03-22 12:21:38 +0800

    1

--- hellogit/hellogit/AppDelegate.m
+++ hellogit/hellogit/AppDelegate.m
@@ -17,6 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"nihao ");
+    
+    
+    
+    
     return YES;
 }
 
